25fb16d30c44f18dfbba55fcc9c89c0310b8808e
bartek-sant <bartek.sant@gmail.com>
2018-02-19 14:32:19 +0100

    Dodaje projekt HMS

---
+++ hms-api-gateway/src/main/resources/static/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+/* App Module */
+var petClinicApp = angular.module('hmsApp', [
+    'ui.router', 'layoutNav', 'layoutFooter', 'layoutWelcome']);
+
+petClinicApp.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', '$httpProvider', function(
+    $stateProvider, $urlRouterProvider, $locationProvider, $httpProvider) {
+
+    // safari turns to be lazy sending the Cache-Control header
+    $httpProvider.defaults.headers.common["Cache-Control"] = 'no-cache';
+
+    $locationProvider.hashPrefix('!');
+
+    $urlRouterProvider.otherwise('/welcome');
+    $stateProvider
+        .state('app', {
+            abstract: true,
+            url: '',
+            template: '<ui-view></ui-view>'
+        })
+        .state('welcome', {
+            parent: 'app',
+            url: '/welcome',
+            template: '<layout-welcome></layout-welcome>'
+        });
+}]);
+
+['welcome', 'nav', 'footer'].forEach(function(c) {
+    var mod = 'layout' + c.toUpperCase().substring(0, 1) + c.substring(1);
+    angular.module(mod, []);
+    angular.module(mod).component(mod, {
+        templateUrl: "scripts/fragments/" + c + ".html"
+    });
+});
