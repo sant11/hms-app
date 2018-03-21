963608713663d8bd70a2c7fc6f1dbcc8db0117c6
sant11 <bartek.sant@gmail.com>
2018-03-21 12:49:40 +0100

    paginator, fix

--- hms-api-gateway/src/app/users/users.component.ts
+++ hms-api-gateway/src/app/users/users.component.ts
@@ -33,6 +33,8 @@ export class UsersComponent implements OnInit {
 */
 import {Component, ViewChild} from '@angular/core';
 import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 /**
  * @title Data table with sorting, pagination, and filtering.
@@ -45,19 +47,42 @@ import {MatPaginator, MatSort, MatTableDataSource} from '@angular/material';
 
 export class UsersComponent {
 //export class TableOverviewExample {
-  displayedColumns = ['id', 'name', 'progress', 'color'];
-  dataSource: MatTableDataSource<UserData>;
+  displayedColumns = ['id', 'login', 'firstName', 'lastName', 'email'];
+  // dataSource: MatTableDataSource<UserData>;
+  dataSource: MatTableDataSource<User>;
+
+  users: User[];
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() {
-    // Create 100 users
-    const users: UserData[] = [];
-    for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
+  constructor(private userService: UserService ) {
 
-    // Assign the data to the data source for the table to render
-    this.dataSource = new MatTableDataSource(users);
+    this.users = [];
+    
+    // // Create 100 users
+    // const users: UserData[] = [];
+    // for (let i = 1; i <= 100; i++) { users.push(createNewUser(i)); }
+
+    // // Assign the data to the data source for the table to render
+    // this.dataSource = new MatTableDataSource(this.users);
+    this.dataSource = new MatTableDataSource(this.users);
+  }
+
+  ngOnInit() {
+    this.getUsers();
+  }
+
+  getUsers(): void {
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+      this.dataSource = new MatTableDataSource(users);
+    });
+  }
+  
+  delete(user: User): void {
+    this.users = this.users.filter(u => u !== user);
+    this.userService.deleteUser(user).subscribe();
   }
 
   /**
@@ -77,29 +102,29 @@ export class UsersComponent {
 }
 
 /** Builds and returns a new User. */
-function createNewUser(id: number): UserData {
-  const name =
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
-      NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
+// function createNewUser(id: number): UserData {
+//   const name =
+//       NAMES[Math.round(Math.random() * (NAMES.length - 1))] + ' ' +
+//       NAMES[Math.round(Math.random() * (NAMES.length - 1))].charAt(0) + '.';
 
-  return {
-    id: id.toString(),
-    name: name,
-    progress: Math.round(Math.random() * 100).toString(),
-    color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
-  };
-}
+//   return {
+//     id: id.toString(),
+//     name: name,
+//     progress: Math.round(Math.random() * 100).toString(),
+//     color: COLORS[Math.round(Math.random() * (COLORS.length - 1))]
+//   };
+// }
 
 /** Constants used to fill up our data base. */
-const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
-  'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
-const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
-  'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
-  'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
+// const COLORS = ['maroon', 'red', 'orange', 'yellow', 'olive', 'green', 'purple',
+//   'fuchsia', 'lime', 'teal', 'aqua', 'blue', 'navy', 'black', 'gray'];
+// const NAMES = ['Maia', 'Asher', 'Olivia', 'Atticus', 'Amelia', 'Jack',
+//   'Charlotte', 'Theodore', 'Isla', 'Oliver', 'Isabella', 'Jasper',
+//   'Cora', 'Levi', 'Violet', 'Arthur', 'Mia', 'Thomas', 'Elizabeth'];
 
-export interface UserData {
-  id: string;
-  name: string;
-  progress: string;
-  color: string;
-}
+// export interface UserData {
+//   id: string;
+//   name: string;
+//   progress: string;
+//   color: string;
+// }
